479686387924250e183cf6f06066ef287ed096b8
victorpimenta1405 <victorpimenta1405@gmail.com.>
2025-06-05 23:12:10 -0300

    primeiro commit

---
+++ api/src/main/java/com/example/api/model/Cliente.java
@@ -0,0 +1,19 @@
+package com.example.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cliente {
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+}
